253c05325d43add3902fae30a9637078895ed3e5
SEA\barora <barora@egencia.com>
2018-01-10 16:40:54 -0800

    adding POJOs

---
+++ spring-rest-client-examples-master/src/main/java/bhavya/springframework/springrestclient/bhavya/springframework/springrestclient/SpringRestClientExamplesApplication.java
@@ -0,0 +1,12 @@
+package bhavya.springframework.springrestclient.bhavya.springframework.springrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+	}
+}
